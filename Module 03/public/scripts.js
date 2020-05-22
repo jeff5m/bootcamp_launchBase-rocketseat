93234b591f42ec213b01c4d1ea6aa7a25a2d9f72
jeffpereira <jeff.matheus18@gmail.com>
2020-05-22 17:23:38 -0300

    edited /video route

--- Module 03/public/scripts.js
+++ Module 03/public/scripts.js
@@ -4,12 +4,6 @@ const cards = document.querySelectorAll('.card');
 for (let i = 0; i < cards.length; i++) {
 	cards[i].addEventListener('click', function () {
 		const videoID = cards[i].getAttribute('id');
-		modalOverlay.classList.add('active');
-		modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoID}`;
+		window.location.href = `/video?id=${videoID}`;
 	});
 }
-
-document.querySelector('.close-modal').addEventListener('click', function(){
-	modalOverlay.classList.remove('active');
-	modalOverlay.querySelector('iframe').src ='';
-});
