2baf66072f94a887f99592e66df1243260096153
jeffpereira <jeff.matheus18@gmail.com>
2020-05-22 14:28:58 -0300

    finishing the third class block

---
+++ Module 03/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+	{
+		id: 'GykTLqODQuU',
+		title: 'Formulário animado com JS puro e CSS Animations | Mayk Brito',
+		duration: '57 min',
+		price: 'free'
+	},
+	{
+		id: 'HN1UjzRSdBk',
+		title: 'Desvendando o CSS Grid na prática | Mayk Brito',
+		duration: '36 min',
+		price: 'free'
+	},
+	{
+		id: 'rAzHvYnQ8DY',
+		title: 'Array: Higher Order Functions | Mayk Brito',
+		duration: '54 min',
+		price: 'free'
+	},
+	{
+		id: 'ghTrp1x_1As',
+		title: 'O que é API? REST e RESTful? | Mayk Brito',
+		duration: '33 min',
+		price: 'free'
+	},
+	{
+		id: 'GSqR2i-Pq6o',
+		title: 'Desvendando a variável this no Javascript | Mayk Brito',
+		duration: '48 min',
+		price: 'free'
+	},
+	{
+		id: '2alg7MQ6_sI',
+		title: 'Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito',
+		duration: '33 min',
+		price: 'free'
+	}
+];
--- Module 03/server.js
+++ Module 03/server.js
@@ -2,21 +2,37 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data');
 
 server.use(express.static('public'));
 
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-	express: server
+	express: server,
+	autoescape: false
+
 });
 
 server.get('/', function (req, res) {
-	return res.render('about');
+	const about = {
+		avatar_url: 'https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4',
+		name: 'Mayk Brito',
+		role: 'Instrutor - RocketSeat',
+		description: 'Programador Full Stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="http://rocketseat.com.br" target="_blank">RocketSeat</a>.',
+		links: [
+			{name: 'Github', url: 'https://github.com/maykbrito'},
+			{name: 'Twitter', url: 'https://twitter.com/maykbrito'},
+			{name: 'Linkedin', url: 'https://www.linkedin.com/in/maykbrito'}
+		]
+	};
+
+	return res.render('about', { about: about }); // poderia mandar apenas { about }
 });
 
 server.get('/portifolio', function(req, res) {
-	return res.render('portifolio');
+
+	return res.render('portifolio', {items: videos});
 });
 
 server.listen(5000, function () {
