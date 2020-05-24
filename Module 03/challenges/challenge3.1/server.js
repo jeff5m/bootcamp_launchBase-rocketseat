9274963adf56fdc08497a5b1bb86a7dbacbbe464
jeffpereira <jeff.matheus18@gmail.com>
2020-05-24 14:05:22 -0300

    challenge 3.1 finished

---
+++ Module 03/challenges/challenge3.1/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const nunjuks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set('view engine', 'njk');
+
+nunjuks.configure('views', {
+	express: server,
+	autoescape: false,
+	noCache: true
+});
+
+server.get('/', function (req, res) {
+	return res.render('about');
+});
+
+server.get('/courses', function (req, res) {
+	return res.render('courses');
+});
+
+server.use(function (req, res) {
+	res.status(404).render('not-found');
+});
+
+server.listen(5000, function () {
+	console.log('server is running');
+});
