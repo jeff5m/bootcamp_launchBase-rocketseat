cfa26642735a6060b07b2ea3a42201ae533506e1
jeffpereira <jeff.matheus18@gmail.com>
2020-05-24 14:36:12 -0300

    challenge 3.2 - change folder structure

---
+++ Module 03/challenge/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+	{
+		id: 'starter',
+		title: 'Starter',
+		modules: '5 módulos',
+		price: 'Free'
+	},
+	{
+		id: 'launchbase',
+		title: 'Launch Base',
+		modules: '11 módulos',
+		price: 'Pago'
+	},
+	{
+		id: 'gostack',
+		title: 'Go Stack',
+		modules: '15 módulos',
+		price: 'Pago'
+	},
+];
