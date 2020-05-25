257d5256d375a35bd3f8199845cdc12349d6dbf8
jeffpereira <jeff.matheus18@gmail.com>
2020-05-25 08:52:35 -0300

    RefactoringFoodfy. Config routes and pages

--- Challenges/02 Refactoring Foodfy/server.js
+++ Challenges/02 Refactoring Foodfy/server.js
@@ -17,6 +17,14 @@ server.get('/', function(req, res) {
   return res.render('index')
 })
 
+server.get('/about', function(req, res){
+  return res.render('about')
+})
+
+server.get('/recipes', function(req, res){
+  return res.render('recipes')
+})
+
 server.listen(5000, function() {
   console.log('server is running')
 })
