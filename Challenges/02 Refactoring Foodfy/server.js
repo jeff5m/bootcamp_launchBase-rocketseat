8591096622fae16f50d70ac6950c55e3a27f1c12
jeffpereira <jeff.matheus18@gmail.com>
2020-05-25 08:41:14 -0300

    challenge refactoringFoodfy. Start/conf server

---
+++ Challenges/02 Refactoring Foodfy/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const nunjuks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjuks.configure('views', {
+  express: server,
+  autoescape: false,
+  noCache: true
+})
+
+server.get('/', function(req, res) {
+  return res.render('index')
+})
+
+server.listen(5000, function() {
+  console.log('server is running')
+})
