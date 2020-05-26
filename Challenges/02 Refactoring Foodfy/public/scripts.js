6dcf29719c3ae74cdafdebadf61feac0b778204f
jeffpereira <jeff.matheus18@gmail.com>
2020-05-25 23:47:38 -0300

    change the way to get clicked recipe

--- Challenges/02 Refactoring Foodfy/public/scripts.js
+++ Challenges/02 Refactoring Foodfy/public/scripts.js
@@ -1,14 +1,9 @@
 const recipeCards = document.querySelectorAll('.recipeCard')
 
-let selectedRecipe = 0
-
 for (let i = 0; i < recipeCards.length; i++) {
-	recipeCards[i].setAttribute('index', i)
+	let recipeId = i
 	recipeCards[i].addEventListener('click', function () {
-
-		selectedRecipe = this.getAttribute('index')
-
-		window.location.href=`/recipes/${selectedRecipe}`
-
+		
+		window.location.href=`/recipes/${recipeId}`
 	});
 }
