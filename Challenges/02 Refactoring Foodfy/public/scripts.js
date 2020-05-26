6437510d3a9af877f20febe98cea56db68235790
jeffpereira <jeff.matheus18@gmail.com>
2020-05-26 12:10:59 -0300

    add hide/show button

--- Challenges/02 Refactoring Foodfy/public/scripts.js
+++ Challenges/02 Refactoring Foodfy/public/scripts.js
@@ -3,7 +3,24 @@ const recipeCards = document.querySelectorAll('.recipeCard')
 for (let i = 0; i < recipeCards.length; i++) {
 	let recipeId = i
 	recipeCards[i].addEventListener('click', function () {
-		
-		window.location.href=`/recipes/${recipeId}`
+
+		window.location.href = `/recipes/${recipeId}`
 	});
 }
+
+const buttons = document.getElementsByTagName('button')
+
+for (let button of buttons) {
+	button.addEventListener('click', function () {
+		if (!(button.classList.contains('clicked'))){
+			button.classList.add('clicked')
+			button.innerHTML = 'Mostrar'
+		} else {
+			button.classList.remove('clicked')
+			button.innerHTML = 'Esconder'
+		}
+	})
+}
+
+// adicionar classe ao botão e pelo css, esconder ou mostrar conteudo da tag p 
+// que vem logo apos o elemento com a classe .clicked
