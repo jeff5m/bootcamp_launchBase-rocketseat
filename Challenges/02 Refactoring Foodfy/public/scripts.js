b26593c8b92e0f244194411aa8ed3db9ac518fd0
jeffpereira <jeff.matheus18@gmail.com>
2020-05-25 21:02:07 -0300

    config page recipes/recipe.njk, scripts.js

--- Challenges/02 Refactoring Foodfy/public/scripts.js
+++ Challenges/02 Refactoring Foodfy/public/scripts.js
@@ -1,21 +1,14 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const recipeCards = document.querySelectorAll('.recipeCard');
+const recipeCards = document.querySelectorAll('.recipeCard')
 
-console.log(location.pathname);
+let selectedRecipe = 0
 
 for (let i = 0; i < recipeCards.length; i++) {
+	recipeCards[i].setAttribute('index', i)
 	recipeCards[i].addEventListener('click', function () {
-		const recipeId = recipeCards[i].getAttribute('id');
-		const recipeTitle = recipeCards[i].querySelector('.recipeTitle').innerHTML;
-		const chefName = recipeCards[i].querySelector('.chefName').innerHTML;
 
-		modalOverlay.classList.add('active');
-		modalOverlay.querySelector('img').src = `assets/${recipeId}.png`;
-		modalOverlay.querySelector('h3').innerText = `${recipeTitle}`;
-		modalOverlay.querySelector('p').innerText = `${chefName}`;
+		selectedRecipe = this.getAttribute('index')
+
+		window.location.href=`/recipes/${selectedRecipe}`
+
 	});
 }
-
-document.querySelector('.closeButton').addEventListener('click', function () {
-	modalOverlay.classList.remove('active');
-});
