5d16c7749bf6cb94274855f5f188a869a0816f83
jeffpereira <jeff.matheus18@gmail.com>
2020-06-23 19:39:27 -0300

    finished date calc/classes until challenge 4-4

--- Module 04/classes/instructors.js
+++ Module 04/classes/instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const data = require('./data.json');
-const { age } = require('./utils');
+const { age, date } = require('./utils');
 const Intl = require ('intl');
 
 // show
@@ -77,8 +77,18 @@ exports.edit = function(req, res) {
 	if(!foundInstructor) {
 		return res.send('Instructor not found');
 	}
+	/*
+			instructor.birth = timestamp
+			date(instructor.birth)
+			return yyyy-mm-dd
+	*/
 
-	return res.render('instructors/edit', { instructor: foundInstructor } );
+	const instructor = {
+		...foundInstructor,
+		birth: date(foundInstructor.birth) // return yyyy, month and day without zeros
+	};
+
+	return res.render('instructors/edit', { instructor } );
 };
 
 // delete
