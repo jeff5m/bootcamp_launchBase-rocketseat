06a3bb3d4d9ca3e0529dbcab21c5c1d884a37a96
jeffpereira <jeff.matheus18@gmail.com>
2020-06-28 08:22:37 -0300

    fix data format bug

--- Module 04/classes/utils.js
+++ Module 04/classes/utils.js
@@ -22,9 +22,9 @@ module.exports = {
 		// yyyy
 		const year = date.getUTCFullYear();
 		// mm
-		const month = `0+${date.getUTCMonth() + 1}`.slice(-2);
+		const month = `0${date.getUTCMonth() + 1}`.slice(-2);
 		// dd
-		const day = `0+${date.getUTCDate()}`.slice(-2);
+		const day = `0${date.getUTCDate()}`.slice(-2);
 
 		return(`${year}-${month}-${day}`);
 	}
