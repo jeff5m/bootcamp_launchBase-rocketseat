d01e91af795d95ce7e5df2872b57cbcb2daf2625
jeffpereira <jeff.matheus18@gmail.com>
2020-07-12 14:45:10 -0300

    finish presentation members data

--- Module 04/classes/controllers/members.js
+++ Module 04/classes/controllers/members.js
@@ -3,20 +3,42 @@ const data = require('../data.json');
 const { age, date } = require('../utils');
 
 exports.index = function(req, res) {
-	const members = data.members;
-	let newMembers = new Array();
-	
-	for (let i = 0; i < members.length; i++) {
-		let formatedServices = members[i].services.split(',');
-		
-		newMembers.push({
-			...members[i],
-			services: formatedServices,	
-		});
-	}
-	
-	return res.render('members/index', { newMembers });
+	return res.render('members/index', { members: data.members});
 };
+
+exports.create = function(req, res) {
+	return res.render('members/create');
+};
+
+exports.post = function(req, res) {
+	const keys = Object.keys(req.body);
+	
+	for (let key of keys) {
+		if (req.body[key] == '') {
+			return res.send('Please, fill all the fields!');
+		}	
+	}	
+
+	let birth = Date.parse(req.body.birth);
+	let id = 1;
+	const lastMember = data.members[data.members.length - 1];
+	
+	if(lastMember) {
+		id = lastMember.id + 1;
+	}	
+	
+	data.members.push({
+		id,
+		...req.body,
+		birth,
+	});	
+
+	fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+		if(err) return res.send('Wright file error!');
+
+		return res.redirect(`/members/${id}`);
+	});	
+};	
 
 exports.show = function(req, res) {
 	// req.query = is any query parameters.
@@ -27,58 +49,19 @@ exports.show = function(req, res) {
 
 	const foundMember = data.members.find(function(member){
 		return member.id == id;
-	});
+	});	
 
 	if(!foundMember) {
 		return res.send('Member not found');
-	}
+	}	
 	
 	const member = {
 		...foundMember,
 		age: age(foundMember.birth),
-	};
+	};	
 
 	return res.render('members/show', { member });
-};
-
-exports.post = function(req, res) {
-	const keys = Object.keys(req.body); // return only object keys 
-	
-	for (let key of keys) {
-		if (req.body[key] == '') {
-			return res.send('Please, fill all the fields!');
-		}
-	}
-
-	let { avatar_url, birth, name, services, gender } = req.body;
-
-	birth = Date.parse(birth);
-	const created_at = Date.now();
-	const id = Number(data.members.length + 1);
-
-	
-	data.members.push({
-		id,
-		avatar_url,
-		name,
-		birth,
-		gender,
-		services,
-		created_at
-	});
-
-	fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
-		if(err) return res.send('Wright file error!');
-
-		return res.redirect('/members');
-	});
-
-	// return res.send(req.body);
-};
-
-exports.create = function(req, res) {
-	return res.render('members/create');
-};
+};	
 
 exports.edit = function(req, res) {
 	
