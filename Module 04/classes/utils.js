4398a2241705d167f0cfe2f5e11c236a57850598
jeffpereira <jeff.matheus18@gmail.com>
2020-07-12 15:06:20 -0300

    member data presentation

--- Module 04/classes/utils.js
+++ Module 04/classes/utils.js
@@ -26,7 +26,13 @@ module.exports = {
 		// dd
 		const day = `0${date.getUTCDate()}`.slice(-2);
 
-		return(`${year}-${month}-${day}`);
+		return {
+			day,
+			month,
+			year,
+			/*Format*/ iso: `${year}-${month}-${day}`,
+			birthDay: `${day}/${month}`
+		};
 	}
 };
 
